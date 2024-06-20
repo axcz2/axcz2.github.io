7a96845593fa1479bb7c1bb7ebf83e199a0d7dd6
axcz2 <167478615+axcz2@users.noreply.github.com>
2024-06-21 00:11:38 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('accept-button').addEventListener('click', function(event) {
-    event.preventDefault();
-    window.location.href = 'screamer.html';
-});
-
-document.getElementById('deny-button').addEventListener('click', function() {
-    window.location.href = 'screamer.html';
-});
